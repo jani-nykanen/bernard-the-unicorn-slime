91c10c12ab836ad5e03a47ab83e85db9c774ea5c
Jani Nykänen <jnyknn@gmail.com>
2026-08-20 19:10:22 +0300

    Fixed wall rendering

--- source/wallpiece.ts
+++ source/wallpiece.ts
@@ -34,34 +34,47 @@ export class WallPiece implements DepthObject {
         const v : Vector = isometricProjection(this.pos);
 
         const dx : number = v.x*12;
-        const dy : number = v.y*12 + 1;
+        const dy : number = v.y*12;
 
         switch (this.orientation) {
 
         case WallOrientation.Left:
 
             canvas.drawBitmap(bmp, Flip.None, dx - 12, dy, 24, 0, 12, 16);
+            canvas.drawBitmap(bmp, Flip.None, dx - 12, dy + 2, 24, 0, 12, 16);
 
-            canvas.setDrawColor(...MASTER_PALETTE[1]);  
-            canvas.fillRect(dx - 12, dy + 8, 12, 1) 
+            if (this.neighborhood[7] < 0) {
+
+                canvas.setDrawColor(...MASTER_PALETTE[1]);
+                canvas.fillRect(dx - 12, dy + 12, 12, canvas.height);
+            }
 
             // Outline
             if (this.pos.y > this.neighborhood[3] && this.pos.y > this.neighborhood[7]) {
             
+                const h : number = this.neighborhood[7] < 0 ? canvas.height : 12;
                 canvas.setDrawColor(...MASTER_PALETTE[0]);
-                canvas.fillRect(dx - 12, dy, 1, 12);
+                canvas.fillRect(dx - 12, dy, 1, h);
             }
             break;
 
         case WallOrientation.Right:
 
             canvas.drawBitmap(bmp, Flip.None, dx, dy, 36, 0, 12, 16);
+            canvas.drawBitmap(bmp, Flip.None, dx, dy + 2, 36, 0, 12, 16);
+
+            if (this.neighborhood[5] < 0) {
+
+                canvas.setDrawColor(...MASTER_PALETTE[2]);
+                canvas.fillRect(dx, dy + 12, 12, canvas.height);
+            }
 
             // Outline
             if (this.pos.y > this.neighborhood[1] && this.pos.y > this.neighborhood[5]) {
-            
+                
+                const h : number = this.neighborhood[5] < 0 ? canvas.height : 12;
                 canvas.setDrawColor(...MASTER_PALETTE[0]);
-                canvas.fillRect(dx + 11, dy, 1, 12);
+                canvas.fillRect(dx + 11, dy, 1, h);
             }
             break;
 
